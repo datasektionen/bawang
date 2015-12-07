83886e5802b1e0f20b81caf9ffb54bbeeebdd7db
Yopi <brann.jesper@gmail.com>
2015-12-07 18:12:05 +0100

    Footer stuff

--- components/datafooter/styles.js
+++ components/datafooter/styles.js
@@ -6,21 +6,17 @@ export default {
 		width: "100%",
 		backgroundColor: dconst.colors.background,
 		display: "block",
-		textAlign: "left",
-		column: {
-			width: "25%",
-			display: "inline-block",
-			textAlign: "left"
-		}
+		textAlign: "left"
 	},
 	footer_delta: {
-		padding: "1rem 1rem 1rem 0",
+		margin: "5rem 1rem 5rem 0",
 		width: "25%",
 		height: "150px"
 	},
 	column: {
 		width: "25%",
 		display: "inline-block",
-		textAlign: "left"
+		textAlign: "left",
+		verticalAlign: "top"
 	}
 }
