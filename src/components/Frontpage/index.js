c86f754cdaaeca2455b22c7ca1fe665b42ada405
Andreas Mårtensson <addem1234@gmail.com>
2018-10-08 21:29:51 +0200

    Frontpage fixes

--- src/components/Frontpage/index.js
+++ src/components/Frontpage/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { Title } from 'react-head'
 import classNames from 'classnames/bind'
@@ -9,13 +9,14 @@ import { Translate, English, Swedish } from '../Translate'
 
 import styles from './Frontpage.module.css'
 import skold from './skold.svg'
+import './FixMe.css'
 
 const cx = classNames.bind(styles)
 
 const Frontpage = ({ location, lang }) =>
 <Taitan pathname={location.pathname}>
   {({ title, body, sidebar }) =>
-    <div>
+    <Fragment>
       <Title>
         { title }
       </Title>
@@ -151,11 +152,44 @@ const Frontpage = ({ location, lang }) =>
           }
         </Calypso>
       </div>
-      <div
-        className={cx('content')}
-        dangerouslySetInnerHTML={{__html: sidebar}}
-      />
-    </div>
+      <div className={cx('content')} >
+        <h2 id="sections_intro">Det här är <span>Datasektionen</span></h2>
+        <div id="home_sections">
+          {sidebar.split('<hr>').map(__html =>
+            <div className="col-md-4 home_section" key={__html}>
+              <div className="home_section_icon">
+                <i className="fa" />
+              </div>
+              <div dangerouslySetInnerHTML={{__html}} />
+            </div>
+          )}
+        </div>
+      </div>
+      <div id="footer" class="row">
+        <div class="col-sm-5 col-md-3" id="contact">
+            <p>&nbsp;</p>
+            <p>
+                <strong>Adress</strong>
+            </p>
+            <p>
+                Konglig Datasektionen<br />
+                Fack vid THS<br />
+                100 44 Stockholm
+            </p>
+            <p>
+                <strong>Organisationsnummer</strong>
+            </p>
+            <p>802412 - 7709</p>
+            <p>
+                <a class="action" href="/kontakt">Kontakt</a>
+            </p>
+        </div>
+        <div class="col-sm-6 col-md-9" id="map">
+          <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.0958020405822!2d18.069220116002757!3d59.348048616563695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d6a99ea1e8d%3A0x8637b28fa239bfcb!2sOsquars+backe+21%2C+114+28+Stockholm!5e0!3m2!1sen!2sse!4v1463425310266" width="100%" height="300" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+        </div>
+      </div>
+
+    </Fragment>
   }
 </Taitan>
 
