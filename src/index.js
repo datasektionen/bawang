248e13e6e701ff469c9051248f26156dffd4413b
Andreas Mårtensson <addem1234@gmail.com>
2018-10-08 21:43:48 +0200

    postbuild

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import http from 'http'
 const server = http.createServer(app)
 
 let currentApp = app
-
-server.listen(process.env.PORT || 3000, error => {
+const port = process.env.PORT || 3000
+server.listen(port, error => {
   if (error) {
     console.log(error)
   }
 
-  console.log('🚀 started')
+  console.log('🚀 started on port ', port)
 })
 
 if (module.hot) {
