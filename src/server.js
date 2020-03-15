9868c5ba08739b5673055791f2f3d64cdf2d5f26
Alexander Viklund <viklu@kth.se>
2020-03-15 20:33:04 +0100

    Add meta-tag for google-site-verification

--- src/server.js
+++ src/server.js
@@ -65,6 +65,7 @@ server
 `<!doctype html>
   <html lang="${req.url.startsWith('/en') ? 'en' : 'sv'}">
   <head>
+    <meta name="google-site-verification" content="tIS6TUU0SAJrplJvlVybZ4ATVZlOKAGswiLPLIBWUKs" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
