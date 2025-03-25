5333165afc3503be51ea3cfc11f57a92a005f3da
Mathias Magnusson <mathias@magnusson.space>
2025-03-25 14:17:47 +0100

    No more gugglus mappus trackus (#98)

--- src/components/Frontpage/index.js
+++ src/components/Frontpage/index.js
@@ -215,7 +215,7 @@ const Frontpage = ({ lang }) => {
             <h2 id="sections_intro">Are you an international student?</h2>
             <div className="text-center" id="home_sections">
               <p>
-                Read this website in <a href={addLangToUrl("/", "en")} className="inline_link">English</a>, 
+                Read this website in <a href={addLangToUrl("/", "en")} className="inline_link">English</a>,
                 or go to the website for META internationals and discover everything you need to know as an international student in the CS Chapter.
               </p>
               <p>
@@ -312,9 +312,8 @@ const Frontpage = ({ lang }) => {
               </Translate>
             </div>
 
-            {/* META Location in Google Maps */}
             <div className="col-sm-6 col-md-9" id="map">
-              <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.0958020405822!2d18.069220116002757!3d59.348048616563695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d6a99ea1e8d%3A0x8637b28fa239bfcb!2sOsquars+backe+21%2C+114+28+Stockholm!5e0!3m2!1sen!2sse!4v1463425310266" width="100%" height="300" frameBorder="0" style={{ border: 0 }} allowFullScreen></iframe>
+              <iframe width="100%" height="100%" src="https://www.openstreetmap.org/export/embed.html?bbox=18.069899976253513%2C59.347444217121804%2C18.072898685932163%2C59.348653041378064&amp;layer=mapnik&amp;marker=59.34804863462937%2C18.071399331092834" style={{ border: 0 }}></iframe>
             </div>
           </div>
         </Fragment>
